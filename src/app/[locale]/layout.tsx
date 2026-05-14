6fb291f2a9c74a3d4799ac9655d5cff188878994
Martinmiranda9 <mirandamartin2001@gmail.com>
2026-05-14 17:24:45 -0300

    Modifico color predeterminado

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -148,7 +148,7 @@ export default async function RootLayout({
         />
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem={false}
         >
           <NextIntlClientProvider messages={messages}>
